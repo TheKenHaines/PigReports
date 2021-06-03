2836ff5eb8322724044cde36dd4ceebbaddee0d0
Kenneth Haines <TheKenHaines@gmail.com>
2021-06-03 14:22:16 -0500

    css change

--- src/components/launchform.js
+++ src/components/launchform.js
@@ -32,7 +32,7 @@ class launchform extends React.Component {
         let data = this.state.bind
 
         this.handleInputChange = this.handleInputChange.bind(this);
-        
+
     }
 
     saveToExcel(event) {
@@ -51,190 +51,151 @@ class launchform extends React.Component {
 
     render() {
         return (
-            <div class="col-md-12">
-                <div className="form-row align-items-center">
-                    <h1>PIG Launched/Received Report Form</h1>
-                    <br/>
-                    <form onSubmit={this.handleSubmit}>
-                        <div>
-                            <div className="row">
-                                <div className="col-md-6">
-                                    <label>Product:</label>
-            <select value={this.handleSubmit} onChange={this.handleInputChange} >
-                                            <option>-Select-</option>
-                                            <option value="product">Natural Gas</option>
-                                            <option value="product">Crude Oil</option>
-                                            <option value="product">Other</option>
-                                        </select>
-                                    
-                                </div>
-                                <div className="col-md-6">
-                                <label>Line Size:</label>
-            <input
-            name="linesize" type="number" value={this.state.linesize}
-            onChange={this.handleInputChange} />
-                            
-                                </div>
-
-                            </div>
-                            <br/>
-                            <div className="row">
-                                <div className="col-md-6">
-                            <label>Date Launched:</label>
-            <input
-                                    name="datelaunched"
-                                    type="date"
-                                    value={this.state.datelaunched}
-                                    onChange={this.handleInputChange} />
-                            
-                            </div>
-                            <div className="col-md-6">
-                            <label>Time Launched:</label>
-            <input
-                                    name="timelaunched"
-                                    type="time"
-                                    value={this.state.timelaunched}
-                                    onChange={this.handleInputChange} />
-                            
-                            </div>
-                            </div>
-                            <br/>
-                            <div className="row">
-                                <div className="col-md-6">
-                            <label>Location Launched: </label>
-            <input                  placeholder="location"
-                                    name="locationlaunched"
-                                    type="text"
-                                    value={this.state.locationlaunched}
-                                    onChange={this.handleInputChange} />
-                           
-                            </div>
-                            <div className="col-md-6">
-                            <label>Launched By:</label>
-            <input
-                                    name="launchedby"
-                                    type="text"
-                                    value={this.state.launchedby}
-                                    onChange={this.handleInputChange} />
-                            
-                            </div>
-                                </div>
-                                <br/>
-                            <div className="row">
-                                <div className="col-md-6">
-                                <label> Date Received:</label>
-            <input
-                                    name="datereceived"
-                                    type="date"
-                                    value={this.state.datereceived}
-                                    onChange={this.handleInputChange} />
-                            
-                                </div>
-                                <div className="col-md-6">
-                                <label>Time Received:</label>
-            <input
-                                    name="timereceived"
-                                    type="time"
-                                    value={this.state.timereceived}
-                                    onChange={this.handleInputChange} />
-                            
-                                </div>
-                            </div>
-                            <br/>
-                            
-                            <div className="row">
-                                <div className="col-md-6">
-                                <label>Location Received:</label>
-            <input
-                                    name="locationreceived"
-                                    type="text"
-                                    value={this.state.locationreceived}
-                                    onChange={this.handleInputChange} />
-                            
-                                </div>
-                                
-                            <div className="col-md-6">
-                            <label>Received By:</label>
-            <input
-                                    name="receivedby"
-                                    type="text"
-                                    value={this.state.receivedby}
-                                    onChange={this.handleInputChange} />
-                            
-                            </div>
-                            </div>
-                            <br/>
-                            <div className="row">
-                                <div className="col-md-6">
-                                <label>New or Used PIG:</label>
-            <select value={this.handleSubmit} onChange={this.handleInputChange} >
-                                    <option>-Select-</option>
-                                    <option value="product">New</option>
-                                    <option value="product">Used</option>
-                                </select>
-                                </div>
-                                <div className="col-md-6">
-                                    <label>Brush or Cup</label>
-                                    <select value={this.handleSubmit} onChange={this.handleInputChange}>
-                                        <option>-Select-</option>
-                                        <option value="brushcup">Brush</option>
-                                        <option value="brushcup">Cup</option>
-                                    </select>
-                                </div>
-                            </div>
-                            <br/>
-                            
-                            <div className="row">
-                                <div className="col-md-6">
-                               <label> Product Purity:</label>
-            <input 
-                                    name="purity"
-                                    type="number"
-                                    value={this.state.purity}
-                                    onChange={this.handleInputChange}/>
-                            </div>
-                            <div className="col-md-6">
-                            <label>Contents in receiver:</label>
-            <input
-                                    name="contents"
-                                    type="text"
-                                    value={this.state.contents}
-                                    onChange={this.handleInputChange} />                           
-                            </div>
-                            </div>
-                        <br/>
-                            <div className="row">
-                            <label> Comments:</label>
-            <input
-                                    name="comments"
-                                    type="text"
-                                    value={this.state.comments}
-                                    onChange={this.handleInputChange} />
-                            
-                            </div>
-                            <br/>
-                            <div className="row">
-                            <label>
-                                <div className="mb-1">
-                                    Upload Photo <span className="font-css top">*</span>
-                                    <div className="">
-                                        <input
-                                            name="photo"
-                                            type="file"
-                                            id="file-input"
-                                            required=".jpg"
-                                        />
-                                    </div>
-                                </div>
-                            </label>
-                            </div>
-                               
-
-                        </div>
-
-                    </form>
+            <div className="col-md-12 launch-wrapper">
+                <div className="row">
+                    <h3>Launched/Recieved Report Form</h3>
                 </div>
+                <form onSubmit={this.handleSubmit}>
+                    <div className="row">
+                        <label className="label-launch col-md-6">
+                            Product:
+                        </label>
+                        <label className="label-launch col-md-6">
+                            Line Size:
+                        </label>
+                    </div>
+                    <div className="row">
+                        <div className="col-md-6">
+                            <select className="input-launch" value={this.handleSubmit} onChange={this.handleInputChange}>
+                                <option>-Select-</option>
+                                <option value="product">Natural Gas</option>
+                                <option value="product">Crude Oil</option>
+                                <option value="product">Other</option>
+                            </select>
+                        </div>
+                        <div className="col-md-6">
+                            <input className="input-launch" placeholder="Line Size" name="linesize" type="number" value={this.state.linesize} onChange={this.handleInputChange} />
+                        </div>
+                    </div>
+                    <div className="row">
+                        <label className="label-launch col-md-6">
+                            Date Launched:
+                        </label>
+                        <label className="label-launch col-md-6">
+                            Time Launched:
+                        </label>
+                    </div>
+                    <div className="row">
+                        <div className="col-md-6">
+                            <input className="input-launch" name="datelaunched" type="date" value={this.state.datelaunched} onChange={this.handleInputChange} />
+                        </div>
+                        <div className="col-md-6">
+                            <input className="input-launch" name="timelaunched" type="time" value={this.state.timelaunched} onChange={this.handleInputChange} />
+                        </div>
+                    </div>
+                    <div className="row">
+                        <label className="label-launch col-md-6">
+                            Location Launched:
+                        </label>
+                        <label className="label-launch col-md-6">
+                            Launched By:
+                        </label>
+                    </div>
+                    <div className="row">
+                        <div className="col-md-6">
+                            <input className="input-launch" placeholder="Location Launched" name="locationlaunched" type="text" value={this.state.locationlaunched} onChange={this.handleInputChange} />
+                        </div>
+                        <div className="col-md-6">
+                            <input className="input-launch" placeholder="Launched By" name="launchedby" type="text" value={this.state.launchedby} onChange={this.handleInputChange} />
+                        </div>
+                    </div>
+                    <div className="row">
+                        <label className="label-launch col-md-6">
+                            Date Received:
+                        </label>
+                        <label className="label-launch col-md-6">
+                            Time Received:
+                        </label>
+                    </div>
+                    <div className="row">
+                        <div className="col-md-6">
+                            <input className="input-launch" name="datereceived" type="time" value={this.state.datereceived} onChange={this.handleInputChange} />
+                        </div>
+                        <div className="col-md-6">
+                            <input className="input-launch" name="timereceived" type="time" value={this.state.timereceived} onChange={this.handleInputChange} />
+                        </div>
+                    </div>
+                    <div className="row">
+                        <label className="label-launch col-md-6">
+                            Location Received:
+                        </label>
+                        <label className="label-launch col-md-6">
+                            Received By:
+                        </label>
+                    </div>
+                    <div className="row">
+                        <div className="col-md-6">
+                            <input className="input-launch" placeholder="Location Received" name="locationreceived" type="text" value={this.state.locationreceived} onChange={this.handleInputChange} />
+                        </div>
+                        <div className="col-md-6">
+                            <input className="input-launch" placeholder="Received By" name="receivedby" type="text" value={this.state.receivedby} onChange={this.handleInputChange} />
+                        </div>
+                    </div>
+                    <div className="row">
+                        <label className="label-launch col-md-6">
+                            New Or Used:
+                        </label>
+                        <label className="label-launch col-md-6">
+                            Brush Or Cup:
+                        </label>
+                    </div>
+                    <div className="row">
+                        <div className="col-md-6">
+                            <select className="input-launch" value={this.handleSubmit} onChange={this.handleInputChange}>
+                                <option>-Select-</option>
+                                <option value="product">New</option>
+                                <option value="product">Used</option>
+                            </select>
+                        </div>
+                        <div className="col-md-6">
+                            <select className="input-launch" value={this.handleSubmit} onChange={this.handleInputChange}>
+                                <option>-Select-</option>
+                                <option value="brush">Brush</option>
+                                <option value="cup">Cup</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <label className="label-launch col-md-6">
+                            Product Purity:
+                        </label>
+                        <label className="label-launch col-md-6">
+                            Contents In Receiver:
+                        </label>
+                    </div>
+                    <div className="row">
+                        <div className="col-md-6">
+                            <input className="input-launch" placeholder="Product Purity" name="purity" type="number" value={this.state.purity} onChange={this.handleInputChange} />
+                        </div>
+                        <div className="col-md-6">
+                            <input className="input-launch" placeholder="Contents" name="contents" type="text" value={this.state.contents} onChange={this.handleInputChange} />
+                        </div>
+                    </div>
+                    <div className="row">
+                        <label className="label-launch col-md-6">Comments:</label>
+                    </div>
+                    <div className="col-md-12">
+                        <input className="input-comments-launch" name="comments" type="text" value={this.state.comments} onChange={this.handleInputChange} />
+                    </div>
+                    <div className="row">
+                        <label className="label-launch col-md-6">Upload Photo:</label>
+                    </div>
+                    <div className="row">
+                    <input className="input-launch" name="photo" type="file" id="file-input" required=".jpg"/>
+                    </div>
+                </form>
             </div>
+
         );
     }
 }
